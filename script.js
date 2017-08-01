525d933a843f302b3c2f0cc50848c237cda9038b
Riley Grigg <griggriley@gmail.com>
2017-07-31 18:40:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Riley Grigg</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Everything</a></li>" +
+"<li><a href='classical.html'>Classical Music</a></li>" +
+"<li><a href='hp.html'>Harry Potter</a></li>"
+"</ul>";
